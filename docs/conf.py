2876ef3736b597bf6acc48856f274e026ed13794
Pavel Cisar <pcisar@ibphoenix.cz>
2020-10-09 20:23:14 +0200

    Fix documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # theme further.
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    #'navbar_title': "FDB",
+    #'navbar_title': "Firebird-driver",
 
     # Tab name for entire site. (Default: "Site")
     'navbar_site_name': "Content",
