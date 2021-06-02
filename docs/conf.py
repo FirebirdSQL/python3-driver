6ca25e4c82a0c8981eba706273d24e8db5d43e02
Pavel Cisar <pcisar@ibphoenix.cz>
2021-06-02 12:06:49 +0200

    Release 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,10 +23,10 @@ copyright = '2020, The Firebird Project'
 author = 'Pavel Císař'
 
 # The short X.Y version
-version = '1.1.0'
+version = '1.2.0'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
