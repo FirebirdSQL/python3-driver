faee273b22f3ef7bb7c6fe8ba5242d4f1bfb17ed
F.D.Castel <fdcastel@gmail.com>
2023-05-14 14:08:59 -0300

    Adds -Default switch to restore default environment. Also sets the variables for current session.

--- Set-TestEnvironment.ps1
+++ Set-TestEnvironment.ps1
@@ -1,12 +1,31 @@
 [CmdletBinding()]
 Param(
+    [Parameter(ParameterSetName = 'Default')]
+    [Alias("Local")]
+    [Switch]
+    $Default,
+
+    [Parameter(ParameterSetName = 'Embedded')]
+    [Alias("Engine")]
     [ValidateSet('fb30', 'fb40', 'fb50')] 
     [System.String[]]
-    $Engine = 'fb50')
+    $Embedded = 'fb50'
+)
 
-$rootFolder = Join-Path $env:TEMP 'firebird-driver-tests'
+$environmentFile = './test/environment.txt'
+if ($Default) {
+    # Use default environment (locally installed Firebird). Clear all environment variables.
+    $env:FBTEST_CLIENT_LIBRARY=
+    $env:FBTEST_DATABASE=
 
-@"
-FBTEST_CLIENT_LIBRARY=%TEMP%/firebird-driver-tests/$($Engine)/fbclient.dll
-FBTEST_DATABASE=%TEMP%/firebird-driver-tests/FIREBIRD-DRIVER.$($Engine.ToUpper()).FDB
-"@ | Out-File ./test/environment.txt -Encoding ascii
+    Remove-Item $environmentFile -ErrorAction SilentlyContinue
+} else {
+    # Use embedded environment (Firebird embedded and database in TEMP subfolder)
+    $env:FBTEST_CLIENT_LIBRARY="$($env:TEMP)/firebird-driver-tests/$($Embedded)/fbclient.dll"
+    $env:FBTEST_DATABASE="$($env:TEMP)/firebird-driver-tests/FIREBIRD-DRIVER.$($Embedded.ToUpper()).FDB"
+
+    @"
+    FBTEST_CLIENT_LIBRARY=%TEMP%/firebird-driver-tests/$($Embedded)/fbclient.dll
+    FBTEST_DATABASE=%TEMP%/firebird-driver-tests/FIREBIRD-DRIVER.$($Embedded.ToUpper()).FDB
+"@ | Out-File $environmentFile -Encoding ascii
+}
